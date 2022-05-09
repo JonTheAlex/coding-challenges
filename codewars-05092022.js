81db51922f06836c0718b33fb8febceac1f69912
JonTheAlex <jon.the.alex@gmail.com>
2022-05-09 06:53:47 -0500

    Updated Pig Latin

--- codewars-05092022.js
+++ codewars-05092022.js
@@ -1,5 +1,8 @@
 function pigIt(str){
-    return str.split(' ').map(element => {
-      return element.slice(1,).concat(element.slice(0,1)).concat('ay')
-    }).join(' ')
-  }
+  return str.split(' ').map(element => {
+    if (!(/[a-zA-Z]/.test(element))){
+      return element
+    }
+    return element.slice(1,).concat(element.slice(0,1)).concat('ay')
+  }).join(' ')
+    }
