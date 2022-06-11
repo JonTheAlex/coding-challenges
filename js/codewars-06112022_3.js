fa45447a9704052915c1ff697971e0c93692b566
JonTheAlex <jon.the.alex@gmail.com>
2022-06-11 12:12:11 -0500

    Check Case

---
+++ js/codewars-06112022_3.js
@@ -0,0 +1,11 @@
+function sameCase(a, b){
+    if (!a.match(/[a-z]/i) || !b.match(/[a-z]/i)){
+      return -1 
+    }
+    if ((a === a.toUpperCase() && b === b.toUpperCase())
+        || (a === a.toLowerCase() && b === b.toLowerCase())){
+          return 1;
+    } else {
+      return 0;
+    }
+  }
