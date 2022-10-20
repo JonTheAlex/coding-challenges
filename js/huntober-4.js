cf5ce539a157b77f10e5d95983df1577b5f16576
JonTheAlex <jon.the.alex@gmail.com>
2022-10-19 19:58:09 -0500

    average of an array

---
+++ js/huntober-4.js
@@ -0,0 +1,11 @@
+// getAverage([2,2,2,2]),2)
+// getAverage([1,2,3,4,5]),3)
+// getAverage([1,1,1,1,1,1,1,2]),2)
+
+function getAverage(arr){
+    return Math.floor(arr.reduce((acc, curr) => acc += curr, 0) / arr.length)
+  }
+  
+  console.log(getAverage([2,2,2,2]), 2)
+  console.log(getAverage([1,2,3,4,5]), 3)
+  console.log(getAverage([1,1,1,1,1,1,1,2]), 1)
