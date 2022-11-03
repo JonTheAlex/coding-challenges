1a85f0890efd61e6e68aae17e82cd3222ffe575b
JonTheAlex <jon.the.alex@gmail.com>
2022-11-02 19:37:37 -0500

    last letter compare

---
+++ js/codewars-11022022.js
@@ -0,0 +1,16 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+// last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+// last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+// last('take me to semynak'), ['take', 'me', 'semynak', 'to']
+
+function last(str){
+    return str.split(' ').sort((a, b) => (a.charAt(a.length-1)).localeCompare((b.charAt(b.length-1))))
+  }
+  
+  console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
+  console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
